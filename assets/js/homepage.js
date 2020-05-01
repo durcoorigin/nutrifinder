366bdd6252fa79db1f745919f48d44d3f297c1fd
Thor40 <nicholas.kosik@gmail.com>
2020-04-30 19:28:35 -0500

    search btn links to new html

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -6,7 +6,8 @@ var searchButtonEl = document.querySelector("#search-button");
 // fetches API info
 var getRecipe = function(searchItem) {
     var apiUrl= 'https://api.edamam.com/search?q=' + searchItem + '&app_id=42dd91c3&app_key=caa35ee6d60671cba69188f9d36208c3&from=0&to=3&calories=591-722&health=alcohol-free';
-    
+    localStorage.setItem('apiUrl', apiUrl)
+    window.location="recipe.html"
 	fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
 	
---
+++ assets/js/recipe.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+    var url = localStorage.getItem('apiUrl')  
+    console.log(url);
+};
