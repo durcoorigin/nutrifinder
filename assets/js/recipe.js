f7df89e8bf9b1c3ed9edd02d846e7b885fd3a93d
Thor40 <nicholas.kosik@gmail.com>
2020-05-04 13:25:14 -0500

    search bar on recipe page now works

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,47 +1,45 @@
+// selects recipe title
 var recipeTitleEl = document.querySelector('#recipe-title');
-var url = localStorage.getItem('apiUrl')
+// selects recipe container
 var recipeContainer = document.querySelector('#recipe-container');
+// selects the search bar input
+var searchEl = document.querySelector("#search-bar");
+// selects the search button
+var searchButtonEl = document.querySelector("#search-button");
 
-window.onload = function () {
-    this.console.log(url);
-    this.getTitle();
-    this.getRecipe();
-};
-
-var getTitle = function(searchItem) {
-    var split = url.split('=');
-    var splitUrl = split[1].split('&')
-    var searchItem = splitUrl[0]
-    console.log(searchItem);
-    recipeTitleEl.textContent = searchItem;
-};
-
-// Function to handle search terms
+// *** search term handler ***
 var searchHandler = function() {
     event.preventDefault();
     // set searchItem to input value
     var searchItem = searchEl.value.trim();
     // if searchItem is true run getRecipe function with values from the search input field and set the input field back to blank
     if(searchItem) {
-        url = 'https://api.edamam.com/search?q=' + searchItem + '&app_id=42dd91c3&app_key=caa35ee6d60671cba69188f9d36208c3&from=0&to=3&calories=591-722&health=alcohol-free'
-        getRecipe(url);
+        getRecipe(searchItem);
         searchEl.value = ""
     }
 };
 
-// --------------------------------------
+// *** Displays title ***
+var getTitle = function(recipeData) {
+    console.log(recipeData.q)
+    recipeTitleEl.textContent = recipeData.q;
+};
 
-// fetches API info
-var getRecipe = function() {
-    var apiUrl= url;
+// *** fetches API info ***
+var getRecipe = function(searchItem) {
+    // **!NOTICE!**
+    // The APP ID and APP KEY in the URL is Nick's (Changed from final)
+    var apiUrl= 'https://api.edamam.com/search?q=' + searchItem + '&app_id=42dd91c3&app_key=caa35ee6d60671cba69188f9d36208c3&from=0&to=3&calories=591-722&health=alcohol-free';
 	fetch(apiUrl).then(function(response) {
         response.json().then(function(recipeData) {
             console.log(recipeData)
             displayRecipe(recipeData)
+            getTitle(recipeData)
         });
     });    
 }
 
+// *** display recipe cards ***
 var displayRecipe = function(recipeData) {
 
     for (var i = 0; i < recipeData.hits.length; i++) {
@@ -77,6 +75,7 @@ var displayRecipe = function(recipeData) {
             var serveLbl = "Number of Servings: ";
             var serve = recipeData.hits[0].recipe.yield;
             var serveEl = document.createElement("h3");
+            serveEl.setAttribute('id', "recipe-title");
             serveEl.textContent = serveLbl + serve;
             nutritionEL.appendChild(serveEl);
 
@@ -169,257 +168,22 @@ var displayRecipe = function(recipeData) {
             nutritionEL.appendChild(proteinEl);
 
         }
-    
-    //     //  ****** Recipe 2 ******
-    //     if (allRecipies[1]) {
-
-    //         //  ****** Recipe Container ******
-    //         var RContainer = document.createElement('div');
-    //         RContainer.classList = "column is-3 box";
-    //         RContainer.setAttribute('id', "container-two")
-    //         recipeContainer.appendChild(RContainer);
-
-    //         //  ****** Display Title ******   
-    //         var title = recipeData.hits[1].recipe.label;   
-    //         var titleEl = document.createElement("h2");
-    //         titleEl.classList = "subtitle";
-    //         titleEl.textContent = title;
-    //         RContainer.appendChild(titleEl);
-
-    //         //  ****** Display image ******
-    //         var image = recipeData.hits[1].recipe.image;
-    //         var imageEl = document.createElement("img");
-    //         var imageURL = document.createElement("a")
-    //         imageURL.setAttribute('href', recipeData.hits[1].recipe.url)
-    //         imageEl.classList = "image";
-    //         imageURL.appendChild(imageEl);
-    //         imageEl.setAttribute('src', image);
-    //         RContainer.appendChild(imageURL);
-
-    //         // ****** Display Nutrition Informarion ******
-    //         var nutritionEL = document.createElement("div");
-    //         nutritionEL.id = "nutrition-container";
-    //         RContainer.appendChild(nutritionEL);
-
-    //         var serveLbl = "Number of Servings: ";
-    //         var serve = recipeData.hits[1].recipe.yield;
-    //         var serveEl = document.createElement("h4");
-    //         serveEl.textContent = serveLbl + serve;
-    //         nutritionEL.appendChild(serveEl);
-
-    //         var amtPerServ = "Amount per serving:"
-    //         var amtPerServEl = document.createElement("h4");
-    //         amtPerServEl.textContent = amtPerServ;
-    //         nutritionEL.appendChild(amtPerServEl);
-
-    //         var calorieLbl = "Calories: ";
-    //         var calorie = recipeData.hits[1].recipe.calories;
-    //         var calorieEl = document.createElement("li");
-    //         calorieEl.textContent = calorieLbl +  Math.round(calorie / serve);
-    //         nutritionEL.appendChild(calorieEl);
-
-    //         var totalFatLbl = "Total Fat: ";
-    //         var totalFat = recipeData.hits[1].recipe.totalNutrients.FAT.quantity;
-    //         var totalFatUnit = recipeData.hits[1].recipe.totalNutrients.FAT.unit;
-    //         var totalFatEl = document.createElement("li");
-    //         totalFatEl.textContent = totalFatLbl +  Math.round(totalFat / serve) + totalFatUnit;
-    //         nutritionEL.appendChild(totalFatEl);
-
-    //         var satFatLbl = "Saturated Fat: ";
-    //         var satFat = recipeData.hits[1].recipe.totalNutrients.FASAT.quantity;
-    //         var satFatUnit = recipeData.hits[1].recipe.totalNutrients.FASAT.unit;
-    //         var satFatEl = document.createElement("li");
-    //         satFatEl.textContent = satFatLbl +  Math.round(satFat / serve) + satFatUnit;
-    //         nutritionEL.appendChild(satFatEl);
-
-    //         var transFatLbl = "Trans Fat: ";
-    //         var transFat = recipeData.hits[1].recipe.totalNutrients.FATRN.quantity;
-    //         var transFatUnit = recipeData.hits[1].recipe.totalNutrients.FATRN.unit;
-    //         var transFatEl = document.createElement("li");
-    //         transFatEl.textContent = transFatLbl +  Math.round(transFat / serve) + transFatUnit;
-    //         nutritionEL.appendChild(transFatEl);
-
-    //         var monoFatLbl = "Monounsaturated Fat: ";
-    //         var monoFat = recipeData.hits[1].recipe.totalNutrients.FAMS.quantity;
-    //         var monoFatUnit = recipeData.hits[1].recipe.totalNutrients.FAMS.unit;
-    //         var monoFatEl = document.createElement("li");
-    //         monoFatEl.textContent = monoFatLbl +  Math.round(monoFat / serve) + monoFatUnit;
-    //         nutritionEL.appendChild(monoFatEl);
-
-    //         var polyFatLbl = "Polyunsaturated Fat: ";
-    //         var polyFat = recipeData.hits[1].recipe.totalNutrients.FAPU.quantity;
-    //         var polyFatUnit = recipeData.hits[1].recipe.totalNutrients.FAPU.unit;
-    //         var polyFatEl = document.createElement("li");
-    //         polyFatEl.textContent = polyFatLbl +  Math.round(polyFat / serve) + polyFatUnit;
-    //         nutritionEL.appendChild(polyFatEl);
-
-    //         var choleLbl = "Cholesterol: ";
-    //         var chole = recipeData.hits[1].recipe.totalNutrients.CHOLE.quantity;
-    //         var choleUnit = recipeData.hits[1].recipe.totalNutrients.CHOLE.unit;
-    //         var choleEl = document.createElement("li");
-    //         choleEl.textContent = choleLbl +  Math.round(chole / serve) + choleUnit;
-    //         nutritionEL.appendChild(choleEl);
-            
-    //         var sodiumLbl = "Sodium: ";
-    //         var sodium = recipeData.hits[1].recipe.totalNutrients.NA.quantity;
-    //         var sodiumUnit = recipeData.hits[1].recipe.totalNutrients.NA.unit;
-    //         var sodiumEl = document.createElement("li");
-    //         sodiumEl.textContent = sodiumLbl +  Math.round(sodium / serve) + sodiumUnit;
-    //         nutritionEL.appendChild(sodiumEl);
-
-    //         var carbLbl = "Total Carbohydrate: ";
-    //         var carb = recipeData.hits[1].recipe.totalNutrients.CHOCDF.quantity;
-    //         var carbUnit = recipeData.hits[1].recipe.totalNutrients.CHOCDF.unit;
-    //         var carbEl = document.createElement("li");
-    //         carbEl.textContent = carbLbl +  Math.round(carb / serve) + carbUnit;
-    //         nutritionEL.appendChild(carbEl);
-
-    //         var fiberLbl = "Dietary Fiber: ";
-    //         var fiber = recipeData.hits[1].recipe.totalNutrients.FIBTG.quantity;
-    //         var fiberUnit = recipeData.hits[1].recipe.totalNutrients.FIBTG.unit;
-    //         var fiberEl = document.createElement("li");
-    //         fiberEl.textContent = fiberLbl +  Math.round(fiber / serve) + fiberUnit;
-    //         nutritionEL.appendChild(fiberEl);
-
-    //         var sugarLbl = "Sugars: ";
-    //         var sugar = recipeData.hits[1].recipe.totalNutrients.SUGAR.quantity;
-    //         var sugarUnit = recipeData.hits[1].recipe.totalNutrients.SUGAR.unit;
-    //         var sugarEl = document.createElement("li");
-    //         sugarEl.textContent = sugarLbl +  Math.round(sugar / serve) + sugarUnit;
-    //         nutritionEL.appendChild(sugarEl);
-
-    //         var proteinLbl = "Protein: ";
-    //         var protein = recipeData.hits[1].recipe.totalNutrients.PROCNT.quantity;
-    //         var proteinUnit = recipeData.hits[1].recipe.totalNutrients.PROCNT.unit;
-    //         var proteinEl = document.createElement("li");
-    //         proteinEl.textContent = proteinLbl +  Math.round(protein / serve) + proteinUnit;
-    //         nutritionEL.appendChild(proteinEl);
-    //     };
-
-    //     //  ****** Recipe 2 ******
-    //     if (allRecipies[2]) {
-
-    //         //  ****** Recipe Container ******
-    //         var RContainer = document.createElement('div');
-    //         RContainer.classList = "column is-3 box";
-    //         RContainer.setAttribute('id', "container-two")
-    //         recipeContainer.appendChild(RContainer);
-
-    //         //  ****** Display Title ******   
-    //         var title = recipeData.hits[2].recipe.label;   
-    //         var titleEl = document.createElement("h2");
-    //         titleEl.classList = "subtitle";
-    //         titleEl.textContent = title;
-    //         RContainer.appendChild(titleEl);
-
-    //         //  ****** Display image ******
-    //         var image = recipeData.hits[2].recipe.image;
-    //         var imageEl = document.createElement("img");
-    //         var imageURL = document.createElement("a")
-    //         imageURL.setAttribute('href', recipeData.hits[1].recipe.url)
-    //         imageEl.classList = "image";
-    //         imageURL.appendChild(imageEl);
-    //         imageEl.setAttribute('src', image);
-    //         RContainer.appendChild(imageURL);
-
-    //         // ****** Display Nutrition Informarion ******
-    //         var nutritionEL = document.createElement("div");
-    //         nutritionEL.id = "nutrition-container";
-    //         RContainer.appendChild(nutritionEL);
-
-    //         var serveLbl = "Number of Servings: ";
-    //         var serve = recipeData.hits[2].recipe.yield;
-    //         var serveEl = document.createElement("h4");
-    //         serveEl.textContent = serveLbl + serve;
-    //         nutritionEL.appendChild(serveEl);
-
-    //         var amtPerServ = "Amount per serving:"
-    //         var amtPerServEl = document.createElement("h4");
-    //         amtPerServEl.textContent = amtPerServ;
-    //         nutritionEL.appendChild(amtPerServEl);
-
-    //         var calorieLbl = "Calories: ";
-    //         var calorie = recipeData.hits[2].recipe.calories;
-    //         var calorieEl = document.createElement("li");
-    //         calorieEl.textContent = calorieLbl +  Math.round(calorie / serve);
-    //         nutritionEL.appendChild(calorieEl);
-
-    //         var totalFatLbl = "Total Fat: ";
-    //         var totalFat = recipeData.hits[2].recipe.totalNutrients.FAT.quantity;
-    //         var totalFatUnit = recipeData.hits[2].recipe.totalNutrients.FAT.unit;
-    //         var totalFatEl = document.createElement("li");
-    //         totalFatEl.textContent = totalFatLbl +  Math.round(totalFat / serve) + totalFatUnit;
-    //         nutritionEL.appendChild(totalFatEl);
-
-    //         var satFatLbl = "Saturated Fat: ";
-    //         var satFat = recipeData.hits[2].recipe.totalNutrients.FASAT.quantity;
-    //         var satFatUnit = recipeData.hits[2].recipe.totalNutrients.FASAT.unit;
-    //         var satFatEl = document.createElement("li");
-    //         satFatEl.textContent = satFatLbl +  Math.round(satFat / serve) + satFatUnit;
-    //         nutritionEL.appendChild(satFatEl);
-
-    //         var transFatLbl = "Trans Fat: ";
-    //         var transFat = recipeData.hits[2].recipe.totalNutrients.FATRN.quantity;
-    //         var transFatUnit = recipeData.hits[2].recipe.totalNutrients.FATRN.unit;
-    //         var transFatEl = document.createElement("li");
-    //         transFatEl.textContent = transFatLbl +  Math.round(transFat / serve) + transFatUnit;
-    //         nutritionEL.appendChild(transFatEl);
-
-    //         var monoFatLbl = "Monounsaturated Fat: ";
-    //         var monoFat = recipeData.hits[2].recipe.totalNutrients.FAMS.quantity;
-    //         var monoFatUnit = recipeData.hits[2].recipe.totalNutrients.FAMS.unit;
-    //         var monoFatEl = document.createElement("li");
-    //         monoFatEl.textContent = monoFatLbl +  Math.round(monoFat / serve) + monoFatUnit;
-    //         nutritionEL.appendChild(monoFatEl);
-
-    //         var polyFatLbl = "Polyunsaturated Fat: ";
-    //         var polyFat = recipeData.hits[2].recipe.totalNutrients.FAPU.quantity;
-    //         var polyFatUnit = recipeData.hits[2].recipe.totalNutrients.FAPU.unit;
-    //         var polyFatEl = document.createElement("li");
-    //         polyFatEl.textContent = polyFatLbl +  Math.round(polyFat / serve) + polyFatUnit;
-    //         nutritionEL.appendChild(polyFatEl);
-
-    //         var choleLbl = "Cholesterol: ";
-    //         var chole = recipeData.hits[2].recipe.totalNutrients.CHOLE.quantity;
-    //         var choleUnit = recipeData.hits[2].recipe.totalNutrients.CHOLE.unit;
-    //         var choleEl = document.createElement("li");
-    //         choleEl.textContent = choleLbl +  Math.round(chole / serve) + choleUnit;
-    //         nutritionEL.appendChild(choleEl);
-            
-    //         var sodiumLbl = "Sodium: ";
-    //         var sodium = recipeData.hits[2].recipe.totalNutrients.NA.quantity;
-    //         var sodiumUnit = recipeData.hits[2].recipe.totalNutrients.NA.unit;
-    //         var sodiumEl = document.createElement("li");
-    //         sodiumEl.textContent = sodiumLbl +  Math.round(sodium / serve) + sodiumUnit;
-    //         nutritionEL.appendChild(sodiumEl);
-
-    //         var carbLbl = "Total Carbohydrate: ";
-    //         var carb = recipeData.hits[2].recipe.totalNutrients.CHOCDF.quantity;
-    //         var carbUnit = recipeData.hits[2].recipe.totalNutrients.CHOCDF.unit;
-    //         var carbEl = document.createElement("li");
-    //         carbEl.textContent = carbLbl +  Math.round(carb / serve) + carbUnit;
-    //         nutritionEL.appendChild(carbEl);
-
-    //         var fiberLbl = "Dietary Fiber: ";
-    //         var fiber = recipeData.hits[2].recipe.totalNutrients.FIBTG.quantity;
-    //         var fiberUnit = recipeData.hits[2].recipe.totalNutrients.FIBTG.unit;
-    //         var fiberEl = document.createElement("li");
-    //         fiberEl.textContent = fiberLbl +  Math.round(fiber / serve) + fiberUnit;
-    //         nutritionEL.appendChild(fiberEl);
-
-    //         var sugarLbl = "Sugars: ";
-    //         var sugar = recipeData.hits[2].recipe.totalNutrients.SUGAR.quantity;
-    //         var sugarUnit = recipeData.hits[2].recipe.totalNutrients.SUGAR.unit;
-    //         var sugarEl = document.createElement("li");
-    //         sugarEl.textContent = sugarLbl +  Math.round(sugar / serve) + sugarUnit;
-    //         nutritionEL.appendChild(sugarEl);
-
-    //         var proteinLbl = "Protein: ";
-    //         var protein = recipeData.hits[2].recipe.totalNutrients.PROCNT.quantity;
-    //         var proteinUnit = recipeData.hits[2].recipe.totalNutrients.PROCNT.unit;
-    //         var proteinEl = document.createElement("li");
-    //         proteinEl.textContent = proteinLbl +  Math.round(protein / serve) + proteinUnit;
-    //         nutritionEL.appendChild(proteinEl);
-    //     };
-    // }
 };
+
+// ************** if you want the click to work on enter key ***************************************************
+// *This is purely a feature, but I thought I'd leave it in here if you wanted to review how it works*         |
+//-                                                                                                            |
+// // Execute a function when the user releases a key on the keyboard                                          |
+// searchButtonEl.addEventListener("keyup", function(event) {                                                  |
+//     // Number 13 is the "Enter" key on the keyboard                                                         |
+//     if (event.keyCode === 13) {                                                                             |
+//       // Cancel the default action, if needed                                                               |
+//       event.preventDefault();                                                                               |
+//       // Trigger the button element with a click                                                            |
+//       document.getElementById("search-button").click(searchHandler);                                        |
+//     }                                                                                                       |
+//   });                                                                                                       |
+// *************************************************************************************************************
+
+// when user clicks the search button it runs the search handler function
+searchButtonEl.addEventListener('click', searchHandler);
